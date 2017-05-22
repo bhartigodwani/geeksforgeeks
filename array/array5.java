b87e13b76ae8c27d1ead4bf53c5bea91918f4a51
bhartigodwani <bhartigodwani@gmail.com>
2017-05-22 16:39:06 +0530

    find a missing number among (1 to (n-1)) numbers

--- array/array5.java
+++ array/array5.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 /*Find the Missing Number
 You are given a list of n-1 integers and these integers are in the range of 1 to n. There are no duplicates in list. One of the integers is missing in the list. Write an efficient code to find the missing integer.
@@ -15,32 +16,44 @@ Solution 2 = (count sum of all numbers from 1 to n) - (count of given numbers)*/
 public class Array5 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int temp[] = new int[10];
+		/*int n = sc.nextInt();
 		int arr[] = new int[n];
-		int flag = 0;
-		int count [] = new int[n+2];
-		int sum = 0;
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
+		}*/
+		
+		int arr[] = {5, 6, 7, 8, 9, 10, 1, 2, 3};
+		
+		firstWay(arr);//using sum of the array
+		secondWay(arr);//using xor operation
+		
+	}
+	
+	static void firstWay(int []arr){
+	    System.out.println("firstWay");
+	    int sum = 0;
+		int n = arr.length;
+		for (int i = 0; i < arr.length; i++) {
 			sum = sum + arr[i];
 		}
 		
-		System.out.println(sum);
 		int total = ((n+1) * (n+2)) / 2;//n is 1 number less than the total number thats y n+1 is taken
-		System.out.println(total);
 		int result = total - sum ;
-		System.out.println("result = "+result);
-		 
-		/*for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
-			count[arr[i]]++;
-		}
-		for (int i = 1; i < count.length; i++) {
-			if (count[i] == 0) {
-				System.out.println("result = "+i);
-				break;
-			}
-		}*/
+		System.out.println("missing element is = "+result+"\n");
+	}
+	
+	static void secondWay(int []a){
+	    System.out.println("secondWay");
+        int max = Arrays.stream(a).max().getAsInt();
+        int X1 = 1;
+        int i ;
+        for(i=2; i<=max;i++)
+            X1 ^= i;
+            
+	    int X2 = a[0];
+	    for (i=1; i<a.length; i++){
+	        X2 ^= a[i];
+	    } 
+	    System.out.println("missing element is = "+(X1 ^ X2));
 	}
 }
