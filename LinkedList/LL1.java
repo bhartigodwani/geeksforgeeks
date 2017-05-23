ccfd5c74805df42d3f3fea4c2bfb8e4c01660c82
bhartigodwani <bhartigodwani@gmail.com>
2017-05-23 13:23:03 +0530

    LL create, all type deletion and insertion

--- LinkedList/LL1.java
+++ LinkedList/LL1.java
@@ -1,26 +1,19 @@
 /*
-  Insert Node at the beginning of a linked list 
-  head pointer input could be NULL as well for empty list
-  Node is defined as 
-  class Node {
-     int data;
-     Node next;
-  }
+Linked List operations implemented:
+
+Creation
+
+Deletion at:
+1. front
+2. end
+3. given position
+4. give key
+
+Insertion at:
+1. front
+2. end
+3. given position
 */
-
-// This is a "method-only" submission. 
-
-/*
-  Insert Node at the beginning of a linked list 
-  head pointer input could be NULL as well for empty list
-  Node is defined as 
-  class Node {
-     int data;
-     Node next;
-  }
-*/
-
-// This is a "method-only" submission. 
 
 class LinkedList{
     static Node head;
@@ -81,8 +74,7 @@ class LinkedList{
         }
         System.out.println("null\n");
         
-        /*deletion of a node given a key*/
-        
+
         /*insertion at front*/
         int valueToBeInsertedAtFront = 5;
         insertAtFront(valueToBeInsertedAtFront);
@@ -119,7 +111,19 @@ class LinkedList{
             traversePtr = traversePtr.link;
         }
         System.out.println("null\n");
+       
         
+        /*deletion of a node given a key*/
+        int key = 6;
+        deleteGivenKey(key);
+        System.out.println("key to be deleted = "+key);
+        System.out.println("LinkedList after deletion of node of given key:");
+        traversePtr = head;
+        while(traversePtr != null){
+            System.out.print(traversePtr.data+" --> ");
+            traversePtr = traversePtr.link;
+        }
+        System.out.println("null\n"); 
     }
     
     static void createLinkedList(int []a, Node head){
@@ -149,6 +153,22 @@ class LinkedList{
         while(pos < position-1)
             ptr = ptr.link;
         ptr.link = ptr.link.link;
+    }
+    
+    static void deleteGivenKey(int key){
+        Node current = head;
+        if(current.data == key){
+            head = head.link;
+        }
+        else{
+            Node next = current.link;
+            while(next.data != key){
+                current = current.link;
+                next = next.link;
+            }            
+            
+            current.link = next.link;
+        }
     }
     
     
