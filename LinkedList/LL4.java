8a731d958fbc8005ba904818442ebb75ccb814bc
bhartigodwani <bhartigodwani@gmail.com>
2017-05-23 16:07:23 +0530

    get Nth node in a Linked List

--- LinkedList/LL4.java
+++ LinkedList/LL4.java
@@ -1,29 +1,60 @@
 /*
-  Delete Node at a given position in a linked list 
-  head pointer input could be NULL as well for empty list
-  Node is defined as 
-  class Node {
-     int data;
-     Node next;
-  }
+Write a function to get Nth node in a Linked List
+Write a GetNth() function that takes a linked list and an integer index and returns the data value stored in the node at 
+that index position.
+Example:
+Input:  1->10->30->14,  index = 2
+Output: 30  
+The node at index 2 is 30
 */
-    // This is a "method-only" submission. 
 
-Node Delete(Node head, int position) {
-  if(position == 0){
-      head = head.next;
-  }
-  else{
-      int pos = 1;
-      Node ptr = new Node();
-      ptr = head;
-      while(pos < position){
-        ptr = ptr.next;
-        pos++;
-      } 
-      
-      ptr.next = ptr.next.next;
-  }
-
-    return head;
+class LinkedList{
+    static Node head;
+    
+    static class Node{
+        int data;
+        Node link;
+        Node(int d){
+            data = d;
+            link = null;
+        }
+    }
+    public static void main (String[] args) {
+        int a[] = {1,2,3,4,5,6};
+        LinkedList ll = new LinkedList();
+        ll.head = new Node(a[0]);
+        
+        /*linked list creation*/
+        createLinkedList(a,head);
+        System.out.println("original LinkedList :");
+        Node traversePtr = head;
+        while(traversePtr != null){
+            System.out.print(traversePtr.data+" --> ");
+            traversePtr = traversePtr.link;
+        }
+        System.out.println("null");
+        
+        /*find Nth node with iteration*/
+        traversePtr = head;
+        int length = 0;
+        int N = 4;
+        boolean flag = false;
+        while(traversePtr != null){
+            if (length == N){
+                System.out.println("index = "+N+"\nNth node is = "+traversePtr.data);
+                break;
+            }
+            length++;
+            traversePtr = traversePtr.link;
+        }
+    }
+    
+    static void createLinkedList(int []a, Node head){
+        Node ptr = head;
+        for (int i=1; i<a.length; i++){
+            Node node = new Node(a[i]);
+            ptr.link = node;
+            ptr = ptr.link;
+        }
+    }
 }
