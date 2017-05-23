f86bf133bfe8552b9fd52d83468941f6cf6893cb
bhartigodwani <bhartigodwani@gmail.com>
2017-05-23 19:49:13 +0530

    Create BTree.java

---
+++ Binary Trees/BTree.java
@@ -0,0 +1,26 @@
+class Tree{
+    static class Node{
+        int data;
+        Node leftChild;
+        Node rightChild;
+        Node(int d){
+            data = d;
+            leftChild = null;
+            rightChild = null;
+        }
+    }
+    public static void main (String[] args) {
+        Node rootNode = new Node(1);
+        Node two = new Node(2);
+        Node three = new Node(3);
+        Node four = new Node(4);
+        
+        rootNode.leftChild = two;
+        rootNode.rightChild = three;
+        two.leftChild = four;
+        
+        System.out.println("\t\t\t"+rootNode.data);
+        System.out.println("\t\t"+rootNode.leftChild.data+"\t\t"+rootNode.rightChild.data);
+        System.out.println("\t"+rootNode.leftChild.leftChild.data);
+    }
+}
