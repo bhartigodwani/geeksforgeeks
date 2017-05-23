7a982f6ab8a381043cf010617b121886a6d7fac3
bhartigodwani <bhartigodwani@gmail.com>
2017-05-23 15:10:23 +0530

    Find Length of a Linked List (Iterative & Recursive)

--- LinkedList/LL2.java
+++ LinkedList/LL2.java
@@ -1,33 +1,66 @@
 /*
-  Insert Node at a given position in a linked list 
-  head can be NULL 
-  First element in the linked list is at position 0
-  Node is defined as 
-  class Node {
-     int data;
-     Node next;
-  }
-*/
-    
+Find Length of a Linked List (Iterative and Recursive)
 
-Node InsertNth(Node head, int data, int position) {
-  Node newNode = new Node();
-    newNode.data = data;
-    int pos = 1;
-    Node ptr1 = new Node();
-    ptr1 = head;
-    while(pos < position){
-        ptr1 = ptr1.next;
-        pos++;
+Write a function to count number of nodes in a given singly linked list.
+For example, the function should return 5 for linked list 1->3->1->2->1
+*/
+
+class LinkedList{
+    static Node head;
+    
+    static class Node{
+        int data;
+        Node link;
+        Node(int d){
+            data = d;
+            link = null;
+        }
+    }
+    public static void main (String[] args) {
+        int a[] = {1,2,3,4};
+        LinkedList ll = new LinkedList();
+        ll.head = new Node(a[0]);
+        
+        /*linked list creation*/
+        createLinkedList(a,head);
+        System.out.println("original LinkedList :");
+        Node traversePtr = head;
+        while(traversePtr != null){
+            System.out.print(traversePtr.data+" --> ");
+            traversePtr = traversePtr.link;
+        }
+        System.out.println("null");
+        
+        /*find length with iteration*/
+        traversePtr = head;
+        int length = 0;
+        while(traversePtr != null){
+            length++;
+            traversePtr = traversePtr.link;
+        }
+        System.out.println("\nlength of the LinkedList with iteration is = "+length);
+        
+        /*find length with recursion*/
+        int count = findLength(head);
+        System.out.println("\nlength of the LinkedList with iteration is = "+count);
     }
     
-    if(position == 0){
-        newNode.next = head;
-        head = newNode;
+    static void createLinkedList(int []a, Node head){
+        Node ptr = head;
+        for (int i=1; i<a.length; i++){
+            Node node = new Node(a[i]);
+            ptr.link = node;
+            ptr = ptr.link;
+        }
     }
-    else{
-        newNode.next = ptr1.next;
-        ptr1.next = newNode;
+    
+    static int findLength(Node ptr){
+        int count = 0;
+        if(ptr.link == null)
+            return 1;
+        else{
+            count = 1 + findLength(ptr.link);
+        }
+    return count;
     }
-    return head;
 }
