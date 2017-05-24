93d54c44db86a0233e9c8281d8a624987a9254c6
bhartigodwani <bhartigodwani@gmail.com>
2017-05-24 18:02:52 +0530

    Update BTree2.java

--- Binary Trees/BTree2.java
+++ Binary Trees/BTree2.java
@@ -1,45 +1,42 @@
 import java.util.Queue;
 import java.util.LinkedList;
-
-public class CreateTree{
-
-    class Node{
+class BTree{
+    static class Node{
         int data;
         Node leftChild;
         Node rightChild;
         Node(int d){
-            int data = d;
+            data = d;
             leftChild = rightChild = null;
         }
     }
-	public static Node createTree(int count){
-		Queue<Node> queue=new LinkedList<>();
-		int label = 1;
-		Node root = null;
-		Node node = new Node(label);
-		root = node;
-		label++;
-		queue.add(root);
-		Node left,right;
-		while(label <= count){
-			node = queue.poll();
-			left = new Node(label++);
-			right = new Node(label++);
-			node.left = left;
-			node.right = right;
-			queue.add(left);
-			queue.add(right);
-		}
-		return root;
-	}
-
-	public static void main(String[] args){
-		Node rootNode = createTree(7);
-		BinaryTree bt = new BinaryTree();
-		bt.printTree(rootNode);
-	}
-	
-	static void createTree(Node rootNode){
-	    
-	}
+    
+    public static void main (String[] args) {
+        int size = 7;
+        Node rootNode = createTree(size);
+        displayTree(rootNode);
+    }
+    
+    static Node createTree(int size){
+        Queue<Node> que = new LinkedList();
+        int value = 1;
+        Node rootNode = new Node(value);
+        que.add(rootNode);
+        while(value <= size){
+            Node node = que.poll();
+            Node leftChild = new Node(++value);
+            Node rightChild = new Node(++value);
+            node.leftChild = leftChild;
+            node.rightChild = rightChild;
+            que.add(leftChild);
+            que.add(rightChild);
+        }
+        return rootNode;
+    }
+    
+    static void displayTree(Node rootNode){
+        System.out.println(rootNode.data);
+        System.out.println(rootNode.leftChild.data+"\t"+rootNode.rightChild.data);
+        System.out.println(rootNode.leftChild.leftChild.data+"\t"+rootNode.leftChild.rightChild.data+"\t"+rootNode.rightChild.leftChild.data+"\t"+rootNode.rightChild.rightChild.data);
+    }
 }
