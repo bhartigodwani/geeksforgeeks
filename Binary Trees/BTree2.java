109750ccac2f38666bb1a29402acbe2db36ef53e
bhartigodwani <bhartigodwani@gmail.com>
2017-05-25 08:27:38 +0530

    Inorder, preorder & postorder

--- Binary Trees/BTree2.java
+++ Binary Trees/BTree2.java
@@ -7,22 +7,42 @@ class BTree{
         Node rightChild;
         Node(int d){
             data = d;
-            leftChild = rightChild = null;
+            leftChild = rightChild =null;
         }
     }
     
     public static void main (String[] args) {
-        int size = 7;
-        Node rootNode = createTree(size);
-        displayTree(rootNode);
+        int size = 7;//number of nodes in complete tree
+        
+        /*first tree*/
+        Node rootNode = createBTree(size);
+        System.out.println("First tree");
+        displayBTree(rootNode);
+        System.out.println("\ninOrderTraversal");
+        inOrderTraversal(rootNode);
+        System.out.println("\npreOrderTraversal");
+        preOrderTraversal(rootNode);
+        System.out.println("\npostOrderTraversal");
+        postOrderTraversal(rootNode);
+        
+        /*Second tree*/
+        rootNode = createBTree2();
+        System.out.println("\n\nSecond tree");
+        displayBTree2(rootNode);
+        System.out.println("\ninOrderTraversal");
+        inOrderTraversal(rootNode);
+        System.out.println("\npreOrderTraversal");
+        preOrderTraversal(rootNode);
+        System.out.println("\npostOrderTraversal");
+        postOrderTraversal(rootNode);
     }
     
-    static Node createTree(int size){
-        Queue<Node> que = new LinkedList();
+    static Node createBTree(int size){
+        Queue<Node> que = new LinkedList<Node>();
         int value = 1;
         Node rootNode = new Node(value);
         que.add(rootNode);
-        while(value <= size){
+        while(value < size){
             Node node = que.poll();
             Node leftChild = new Node(++value);
             Node rightChild = new Node(++value);
@@ -34,9 +54,62 @@ class BTree{
         return rootNode;
     }
     
-    static void displayTree(Node rootNode){
+    static void displayBTree(Node rootNode){
         System.out.println(rootNode.data);
         System.out.println(rootNode.leftChild.data+"\t"+rootNode.rightChild.data);
-        System.out.println(rootNode.leftChild.leftChild.data+"\t"+rootNode.leftChild.rightChild.data+"\t"+rootNode.rightChild.leftChild.data+"\t"+rootNode.rightChild.rightChild.data);
+        System.out.print(rootNode.leftChild.leftChild.data+"\t"+rootNode.leftChild.rightChild.data+"\t");
+        System.out.println(rootNode.rightChild.leftChild.data+"\t"+rootNode.rightChild.rightChild.data);
+    }
+    
+    static Node createBTree2(){
+        Node rootNode = new Node(1);
+        Node two = new Node(2);
+        Node three = new Node(3);
+        rootNode.rightChild = two;
+        rootNode.rightChild.leftChild = three;
+        return rootNode;
+    }
+    
+    static void displayBTree2(Node rootNode){
+        System.out.println(rootNode.data);
+        System.out.print(rootNode.rightChild.data+"\n");
+        System.out.println(rootNode.rightChild.leftChild.data);
+    }
+    
+    static void inOrderTraversal(Node node){
+        if(node == null)
+            return ;
+        
+        if(node.leftChild == null && node.rightChild == null){
+            System.out.print(node.data+"\t");
+            return;
+        }
+        inOrderTraversal(node.leftChild);
+        System.out.print(node.data+"\t");
+        inOrderTraversal(node.rightChild);
+    }
+    
+    static void preOrderTraversal(Node node){
+        if(node == null)
+            return ;
+        if(node.leftChild == null && node.rightChild == null){
+            System.out.print(node.data+"\t");
+            return;
+        }
+        System.out.print(node.data+"\t");
+        preOrderTraversal(node.leftChild);
+        preOrderTraversal(node.rightChild);
+    }
+    
+    static void postOrderTraversal(Node node){
+        if(node == null)
+            return ;
+        if(node.leftChild == null && node.rightChild == null){
+            System.out.print(node.data+"\t");
+            return;
+        }
+        postOrderTraversal(node.leftChild);
+        postOrderTraversal(node.rightChild);
+        System.out.print(node.data+"\t");
     }
 }
