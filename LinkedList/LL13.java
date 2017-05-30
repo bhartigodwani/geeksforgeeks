8d9a2c0a033af4ede2d079f101dabf3fcabb0898
bhartigodwani <bhartigodwani@gmail.com>
2017-05-30 11:18:15 +0530

    Update LL13.java

--- LinkedList/LL13.java
+++ LinkedList/LL13.java
@@ -12,6 +12,7 @@ class LinkedList{
     public static void main (String[] args) {
         /*-------------------------------using marked visite-------------------------*/
         /*-------------------------------case 1---------------------------------------*/
+        System.out.println("case 1");
         int size = 7;
         Node head = new Node(1);
         for (int i=2; i<=size; i++)
@@ -23,6 +24,8 @@ class LinkedList{
             System.out.println("loop doesnot exist");
             
         /*-------------------------------case 2---------------------------------------*/
+        /*------------------------------------only one node exist----------------------*/
+        System.out.println("case 2");
         size = 1;
         head = new Node(1);
         for (int i=2; i<=size; i++)
@@ -32,6 +35,27 @@ class LinkedList{
             System.out.println("loop exist");
         else
             System.out.println("loop doesnot exist");
+            
+        /*-------------------------------case 3---------------------------------------*/
+        /*-------------------------------loop exist------------------------------------*/
+        System.out.println("case 3");
+        head = createLoopLinkedList();
+        ifLoop = findLoopVisited(head);
+        if(ifLoop == true)
+            System.out.println("loop exist");
+        else
+            System.out.println("loop doesnot exist");
+        
+        
+        /*-------------------------------case 4---------------------------------------*/
+        /*-------------------------------empty LinkedList-----------------------------*/
+        System.out.println("case 4");
+        head = null;
+        ifLoop = findLoopVisited(head);
+        if(ifLoop == true)
+            System.out.println("loop exist");
+        else
+            System.out.println("loop doesnot exist");    
         
     }
     
@@ -54,10 +78,22 @@ class LinkedList{
                 temp.visited = "yes";
                 temp = temp.link;
             }
-            if(temp.visited.equals("yes"))
+            if(temp != null && temp.visited.equals("yes"))
                 return true;
             else
                 return false;
         }
+    }
+    
+    static Node createLoopLinkedList(){
+        Node head = new Node(1);
+        Node two = new Node(2);
+        Node three = new Node(3);
+        Node four = new Node(4);
+        head.link = two;
+        two.link = three;
+        three.link = four;
+        four.link = two;
+        return head;
     }
 }
