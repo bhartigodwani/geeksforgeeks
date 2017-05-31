92f5f963dd96d7645e4ea4ed7648a2cdd350a755
bhartigodwani <bhartigodwani@gmail.com>
2017-05-31 12:59:50 +0530

    Convert a Binary Tree into its Mirror Tree

---
+++ Binary Trees/BTree13.java
@@ -0,0 +1,127 @@
+/*
+Write an Efficient Function to Convert a Binary Tree into its Mirror Tree
+Mirror of a Tree: Mirror of a Binary Tree T is another Binary Tree M(T) with left and right children of all non-leaf nodes 
+interchanged.
+*/
+
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+class BTree{
+    static class Node{
+        int data;
+        Node leftChild;
+        Node rightChild;
+        Node(int d){
+            data = d;
+            leftChild = rightChild = null;
+        }
+    }
+    
+    public static void main (String[] args) {
+        int size = 7;
+        Node rootNode = createBTree(size);
+        System.out.println("First tree");
+        displayBTree(rootNode);
+        System.out.println("\nmirror tree");
+        rootNode = convertMirror(rootNode);
+        BFSTraversalLineByLine(rootNode);
+        
+        
+        System.out.println("\n");
+        rootNode = createBTree2();
+        System.out.println("Second tree");
+        BFSTraversalLineByLine(rootNode);
+        System.out.println("\nmirror tree");
+        rootNode = convertMirror(rootNode);
+        BFSTraversalLineByLine(rootNode);
+    }
+    
+    static Node convertMirror(Node rootNode){
+        if(rootNode == null)
+            return rootNode;
+            
+        if (rootNode.leftChild == null && rootNode.rightChild == null)
+            return rootNode;
+        
+        Node leftChild = convertMirror(rootNode.leftChild);
+        Node rightChild = convertMirror(rootNode.rightChild);
+        Node temp = leftChild;
+        rootNode.leftChild = rightChild;
+        rootNode.rightChild = temp;
+        return rootNode;
+    }
+
+
+    static Node createBTree(int size){
+        Queue<Node> que = new LinkedList<Node>();
+        int value = 1;
+        Node rootNode = new Node(value);
+        que.add(rootNode);
+        while(value < size){
+            Node node = que.poll();
+            Node leftChild = new Node(++value);
+            Node rightChild = new Node(++value);
+            node.leftChild = leftChild;
+            node.rightChild = rightChild;
+            que.add(leftChild);
+            que.add(rightChild);
+        }
+        return rootNode;
+    }
+    
+    static Node createBTree2(){
+        Node rootNode = new Node(1);
+        Node two = new Node(2);
+        Node three = new Node(3);
+        Node four = new Node(4);
+        Node five = new Node(5);
+        Node six = new Node(6);
+        Node seven = new Node(7);
+        
+        rootNode.leftChild = two;
+        rootNode.rightChild = three;
+        two.leftChild = four;
+        three.leftChild = six;
+        six.rightChild = seven;
+        
+        return rootNode;
+    }
+        
+    static void displayBTree(Node rootNode){
+        System.out.println(rootNode.data);
+        System.out.println(rootNode.leftChild.data+"\t"+rootNode.rightChild.data);
+        System.out.print(rootNode.leftChild.leftChild.data+"\t"+rootNode.leftChild.rightChild.data+"\t");
+        System.out.println(rootNode.rightChild.leftChild.data+"\t"+rootNode.rightChild.rightChild.data);
+    }
+    
+    static void BFSTraversalLineByLine(Node rootNode){
+        int height = findHeight(rootNode);
+        /*print node level wise*/
+        for(int level=1; level<=height; level++){
+            printLevelOrderLineByLine(rootNode, level);
+            System.out.println();
+        }
+    }
+    
+    static void printLevelOrderLineByLine(Node rootNode, int level){
+        if(rootNode == null)
+            return;
+        if(level == 1)
+            System.out.print(rootNode.data+"\t");
+        else if(level > 1){
+            printLevelOrderLineByLine(rootNode.leftChild,level-1);
+            printLevelOrderLineByLine(rootNode.rightChild, level-1);
+        }
+    }
+    
+     static int findHeight(Node node){
+        if (node == null)
+            return 0;
+        if(node.leftChild == null && node.rightChild == null)
+            return 1;
+        int height = Math.max(findHeight(node.leftChild), findHeight(node.rightChild)) + 1;
+        return height;
+    }
+    
+}
