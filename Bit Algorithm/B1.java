a6ec1e50ca93239e0d197bf4c228db4ec46fa1a6
bhartigodwani <bhartigodwani@gmail.com>
2017-06-16 10:52:50 +0530

    Find the element that appears once O(n)

--- Bit Algorithm/B1.java
+++ Bit Algorithm/B1.java
@@ -1,33 +1,29 @@
 /*
 Find the element that appears once
-Given an array where every element occurs three times, except one element which occurs only once. Find the element that occurs once. Expected time complexity is O(n) and O(1) extra space.
+Given an array where every element occurs three times, except one element which occurs only once. Find the element that occurs 
+once. Expected time complexity is O(n) and O(1) extra space.
 Examples:
 
 Input: arr[] = {12, 1, 12, 3, 12, 1, 1, 2, 3, 3}
 Output: 2
-
-Complexity = O(nlogn)
 */
-import java.util.Scanner;
 import java.util.Arrays;
-class Solution{
-    public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int []arr = new int[n];
-        for (int i=0; i<n; i++)
-            arr[i] = sc.nextInt();
-        Arrays.sort(arr);
-        
-        int i;
-        for(i=0; i<n; ){
-            if(arr[i] == arr[i+1] && arr[i+1] == arr[i+2])
-                i=3;
-            else
-                break;
+class Bit{
+    public static void main(String []args){
+        //int[] arr = {10, 12, 20, 30, 25, 40, 32, 31, 35, 50, 60};
+        int[] arr = {12, 1, 12, 3, 12, 1, 1, 2, 3, 3};
+        //int []arr = {5,5,8,5};
+        System.out.println("Array = "+Arrays.toString(arr));
+        int result = 0;
+        int lastBit, sum;
+        for(int j=0; j<32; j++){
+            sum = 0;
+            for(int i=0; i<arr.length; i++){
+                sum += arr[i] & 1;
+                arr[i] >>= 1;
+            }
+            result = (sum%3)<<j | result;
         }
-        
-        System.out.println("element = "+arr[i]);
-        
+        System.out.println("result = "+result);
     }
 }
