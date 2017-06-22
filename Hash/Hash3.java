13aa8644c9a36f5869771df16ac7f4b1d8a5b9f2
bhartigodwani <bhartigodwani@gmail.com>
2017-06-22 10:59:57 +0530

    Find Itinerary from a given list of tickets

---
+++ Hash/Hash3.java
@@ -0,0 +1,40 @@
+/*
+Find Itinerary from a given list of tickets
+Given a list of tickets, find itinerary in order using the given list.
+
+Example:
+Input:
+"Chennai" -> "Banglore"
+"Bombay" -> "Delhi"
+"Goa"    -> "Chennai"
+"Delhi"  -> "Goa"
+
+Output: 
+Bombay->Delhi, Delhi->Goa, Goa->Chennai, Chennai->Banglore,
+*/
+
+import java.util.*;
+class Hash3{
+    public static void main(String []args){
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("Chennai", "Banglore");
+        map.put("Bombay", "Delhi");
+        map.put("Goa", "Chennai");
+        map.put("Delhi", "Goa");
+        String start = "";
+        for(Map.Entry<String,String> entry : map.entrySet()){
+            if(!map.containsValue(entry.getKey())){
+                start = entry.getKey();
+                break;
+            }
+        }
+        
+        System.out.println("start "+start);
+        while(true){
+            System.out.println(start+" -> "+map.get(start));
+            if(!map.containsKey(map.get(start)))
+                break;
+            start = map.get(start);
+        }
+    }
+}
